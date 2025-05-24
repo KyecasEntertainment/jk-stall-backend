86f467b39a4cd2507589b3c177b1d3faae0f7b63
KyecasEntertainment <skyelucasmaddie@gmail.com>
2025-05-24 23:51:06 +0800

    fixed api.php not working

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ProductController;
 
-Route::get('ping', function () {
-    return response()->json(['message' => 'pong']);
+Route::prefix('inventory/')->name('inventory.')->group(function () {
+    Route::post('create-product', [ProductController::class, 'createProduct'])->name('create-product');
+    Route::get('view-products', [ProductController::class, 'viewProducts'])->name('view-products');
+    Route::get('edit-product/{id}', [ProductController::class, 'editProduct'])->name('edit-product');
+    Route::put('update-product/{id}', [ProductController::class, 'updateProduct'])->name('update-product');
 });
--- routes/web.php
+++ routes/web.php
@@ -2,20 +2,18 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
-use App\Http\Controllers\ProductController;
 
 Route::get('/', [UserController::class, 'test']);
-Route::get('/welcome', [UserController::class, 'test'])->name('welcome');
-// Route::post('/inventory/create-batch', [ProductController::class, 'createProduct'])->name('create-batch');
 
 
-Route::prefix('admin/')->name('admin.')->group(function () {
-    Route::get('login', [UserController::class, 'login'])->name('login');
+// Route::prefix('admin/')->name('admin.')->group(function () {
+//     Route::get('login', [UserController::class, 'login'])->name('login');
 
-});
+// });
 
-Route::prefix('inventory/')->name('inventory.')->group(function () {
-    //Route::post('create-batch', [ProductController::class, 'createProduct'])->name('create-product');
-    Route::get('edit-product/{id}', [UserController::class, 'editProduct'])->name('edit-product');
-    Route::put('update-product/{id}', [UserController::class, 'updateProduct'])->name('update-product');
-});
+// Route::prefix('inventory/')->name('inventory.')->group(function () {
+//     Route::post('create-product', [UserController::class, 'createProduct'])->name('create-product');
+//     Route::get('view-products', [UserController::class, 'viewProducts'])->name('view-products');
+//     Route::get('edit-product/{id}', [UserController::class, 'editProduct'])->name('edit-product');
+//     Route::put('update-product/{id}', [UserController::class, 'updateProduct'])->name('update-product');
+// });
